9f9c0b419abf49b3bc9ab95ad3bbeb4789554f61
Sunitha Kolli <sunitha@olivetech.net>
2018-03-28 17:53:16 +0530

    changing the providers names and checking

--- src/app/computer/computer.component.ts
+++ src/app/computer/computer.component.ts
@@ -4,8 +4,11 @@ import { DesktopService } from './services/desktop.service';
 
 @Component({
   selector: 'app-computer',
-  providers : [ LaptopService,
+  /*providers : [ LaptopService,
     { provide: DesktopService, useExisting: LaptopService }
+  ],*/
+  providers : [ DesktopService,
+    { provide: LaptopService, useExisting: DesktopService }
   ],
   template : `<h3> I work on {{computerName}} </h3>`,
  
@@ -13,7 +16,7 @@ import { DesktopService } from './services/desktop.service';
 export class ComputerComponent implements OnInit {
   computerName : string;
 
-  constructor(private ComputerService : DesktopService) { }
+  constructor(private ComputerService  : LaptopService) { }
 
   ngOnInit() {
     this.computerName = this.ComputerService.getComputerName();
